ff99af5df253a5cf1434d66a00b9a237a6f000de
set2333 <set2333@mail.ru>
2019-12-12 19:29:54 +0700

    Алгоритм работает. Утилита выполняет задачу.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const unzip = require('unzip');
 const FILE_FORM_MASK = '_379_Q_G_01.ZIP'; //Как показывае приктика эта строка содержится во всех необходимых нам файлах.
 const RECURSION_COUNT = 2; //Максимальная глубина погружения в рекурсию. При моей структуре папок хватит одного погружения.
+const OFFSET = 48; //В файле хранятся коды символов, но мне удобнее работать со стандартными номерами КФО. Для конвертации используем эту константу.
 const path = process.argv[2];
 
 if (!path) {
@@ -9,56 +10,40 @@ if (!path) {
     process.exit(1);
 }
 
+function isPointKFO(arr, item) { //Определяем, является ли эта строка стокой с номером КФО. Должна начинаться с "КВД=".
+    return ((arr[item] == 202) && (arr[item + 1] == 194) && (arr[item + 2] == 196) && (arr[item + 3] == 61)); 
+}
+
+function isExcessString(arr, item, KFO) { //Проверка на нужность строки. Строка лишняя если она описывает КФО или содержит данные по КФО, но ма создаем файл по другому КФО.
+    return ((isPointKFO(arr[item], 0) && arr[item][4] !== KFO + OFFSET) || (~[49, 50, 51, 52, 53, 54, 55].indexOf(arr[item][0]) && arr[item][0] !== KFO + OFFSET));
+}
+
 function getNewFiles(path, fileName) { //Чтение исходного файла и создание новых файлов на его основе.
     fs.createReadStream(path + '/' + fileName).pipe(unzip.Parse()).on('entry', function (entry) {
         entry.on('data', function (chunk) {
-            console.log(chunk);
-            let data = chunk.toJSON().data;
-            let arrData = [];
-            let arrEndStr = [];
-            let arrStart = 0;
-            for(let i=0; i< chunk.length; i++) {
-                if (chunk[i] == 10) {
-                    arrEndStr.push(+i);
-                }
-            }
-            for(let i=0; i<arrEndStr.length; i++) {
-                arrData.push(data.slice(arrStart, (arrEndStr[i]+1)));
-                arrStart = arrEndStr[i]+1;
-            }
+            let data = chunk.toJSON().data; //Преобразуем данные в массив. Так их легче обработать.
             let arrKFO = []; //массив, в котором мы будем хранить виды КФО найденные в отчете.
-            for(let i=0; i<arrData.length; i++) {
-                if((arrData[i][0] == 202) && (arrData[i][1] == 194) && (arrData[i][2] == 196) && (arrData[i][3] == 61)) {
-                    arrKFO.push(arrData[i][4]-48);
+            let arrData = [];
+            let arrStart = 0; //Указатель на начало текущей строки.
+            //Разобъем данные на строки, преобразуем каждую в Buffer и сложим в массив. В дальнейшем мы сможем решать, нужна ли нам строка, и если нет, то просто удалим её из массива.
+            for (let i = 0; i < data.length; i++) {
+                if (chunk[i] == 10) { // 10 - конец строки
+                    if (isPointKFO(data, i + 1)) { //Если эта строка - строка с описанием КФО, то получем КФО (5 элемент в строка), и записываем его в массив найденных КФО.
+                        arrKFO.push(data[i + 5] - OFFSET);
+                    }
+                    arrData.push(new Buffer.from(data.slice(arrStart, (i + 1))));
+                    arrStart = i + 1; //Обновим указатель. Пусть указывает на новую строку.
                 }
             }
-            console.log(arrKFO);
-            for (let i=0; arrKFO.length; i++) {
-                
+            for (let n = 0; n < arrKFO.length; n++) { //Создаем новый файл для каждого КФО.
+                let newData = arrData.slice();
+                for (let i = 0; i < newData.length; i++) {
+                    if (isExcessString(newData, i, arrKFO[n])) { //Проверяем, не является ли эта строка лишней для данного КФО. Если лишняя - удалим её из массива.
+                        newData.splice(i--, 1);
+                    }
+                }
+                fs.createWriteStream(path + '/379Q01_' + arrKFO[n] + '.txt').end(new Buffer.concat(newData)); //Записываем наш массив буферов в файл.
             }
-//            let arrKFO = [2]; //массив, в котором мы будем хранить виды КФО найденные в отчете
-//            let data = chunk.toJSON().data;
-//            console.log(data);
-//            for (let i = 0; i < data.length; i++) { //Поиск КФО
-//                console.log(data[i]);
-//                if (~data[i].indexOf('JBD=')) {
-//                    arrKFO.push(data[i][4]);
-////                    console.log(data[i]);
-//                }
-//            }
-//            for (let n = 0; n < arrKFO.length; n++) { //Для каждого КФО создадим отдельный файл
-//                let newFile = fs.createWriteStream(path + '/379Q01_' + arrKFO[n] + '.txt');
-//                for (let i = 0; i < data.length; i++) {
-//                    newFile.write(data[i] + '\n');
-////                    if (!((~data[i].indexOf('JBD=') && data[i][4] !== arrKFO[n]) || (~'1234567'.indexOf(data[i][0]) && data[i][0] !== arrKFO[n]))) {
-////                        newFile.write(data[i] + '\n');
-////                    }
-//                }
-//                newFile.end('');
-//            }
-        
-        
-        
         });
     });
 }
